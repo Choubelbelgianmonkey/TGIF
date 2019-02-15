8737b87d7e45289fcd6f9bfb606ea4b3962c3a7d
Pierre Dramaix <pierre.dramaix.java@gmail.com>
2019-02-15 13:48:50 +0100

    15 feb

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,6 +1,13 @@
 var data; //empty var wich will be filled out by a json through the function start(url) below
 var memberList;
 
+$body = $("body");
+
+$(document).on({
+    ajaxStart: function() { $body.addClass("loading");    },
+     ajaxStop: function() { $body.removeClass("loading"); }    
+});
+
 
 if (location.pathname == "/senate-data.html" || location.pathname == "/Senate%20Attendance%20statistics.html" || location.pathname == "/Senate%20Partyl%20Loyalty.html") {
     start("https://api.propublica.org/congress/v1/113/senate/members.json");
@@ -46,7 +53,8 @@ function populateTable(guys) {
 
     tbody.innerHTML = ""; // reset the content of the table for each loop for the function searchParty() & searchState()
 
-    for (i = 0; i < guys.length; i++) {
+    for (i = 0; i < guys.length; i++) 
+    {
 
         var row = table.insertRow(-1); //minus one in ordert o create row from top to bottom
         row.setAttribute("class", ` ${guys[i].party}, ${guys[i].state}`); //to add a class for each row for filter below
@@ -68,7 +76,7 @@ function populateTable(guys) {
         cell3.append(guys[i].state);
         cell4.append(guys[i].seniority);
         cell5.append(guys[i].votes_with_party_pct);
-    }
+    } 
 }
 
 //navbar begin here
@@ -82,7 +90,7 @@ var partyList = []; // this is the list of the party w/o duplicate
 function checkParty() {
 
     //step 1 : create checkboxes
-    var my_checkboxes = document.getElementById("my_checkboxes") //this function start at the ID "my_checkboxes" in                                                                                       // the HTML page
+    var my_checkboxes = document.getElementById("my_checkboxes") //this function start at the ID "my_checkboxes" in                                                                                     // the HTML page
     for (var i = 0; i < memberList.length; i++) { // this loop 
         if (!partyList.includes(memberList[i].party)) { //  merge duplicate    
             partyList.push(memberList[i].party); // and create an array
@@ -92,10 +100,29 @@ function checkParty() {
     if (!partyList.includes("I")) {
         partyList.push("I")
     }
+    
 
     partyList.sort() //i sort them now in order to have a reliable array for the filters function "doubleFilters()"
+    
+       
+            
+         var fullPartyName = partyList.map(party => {
 
-    partyList.forEach(party => { //function inside another function! funception!
+             if (party === "D") {
+                 party = "Democrat"
+             } else if (party === "I") {
+                 party = "Independant"
+             } else {
+                 party = "Republican"
+             }
+             return party // very important! the return allow the value to "leave" the {}
+         })
+
+ 
+    
+
+
+    fullPartyName.forEach(party => { //function inside another function! funception!
         //party here is a variable wich is given to all element in the array
 
         var checkBox = document.createElement("input") //this function
@@ -129,13 +156,17 @@ var stateList = []
 
 function createDropDown() {
     //step 1 : create dropDown
-    var dropDownState = document.getElementById("drop_down_state") //this function start at the ID "dropDownState" in                                                                                       // the HTML page
+    var dropDownState = document.getElementById("drop_down_state"); //this function start at the ID "dropDownState" in  
+                                                                                    // the HTML page
+
 
     for (var i = 0; i < memberList.length; i++) { // this loop 
         if (!stateList.includes(memberList[i].state)) { //  merge duplicate    
             stateList.push(memberList[i].state); // and create an array
         }
     }
+    
+
 
 
     for (var j = 0; j < stateList.length; j++) {
@@ -152,6 +183,7 @@ function createDropDown() {
         label.appendChild(document.createTextNode(stateList[j])); // the aforementioned variables
     }
 }
+
 
 //step 5 create an array wich will contain only row matching search, then pushing them to the first (and therefore last) function populateTable(guys)
 
@@ -197,7 +229,5 @@ function doubleFilters() {
     }
 
     populateTable(laststand)
-
-
 
 }
